83096e4841e0b2fba6af294fb7e43f44d22e981e
Steve Daly <steve.daly@neweratech.com>
2026-01-05 15:06:50 -0500

    feat: export GeminiOCR from package

--- src/word_ocr/__init__.py
+++ src/word_ocr/__init__.py
@@ -2,7 +2,7 @@
 # ABOUTME: Exposes process_document and process_batch functions
 
 from .processor import process_document, process_batch, ProcessResult
-from .ocr import OCREngine, TesseractOCR
+from .ocr import OCREngine, TesseractOCR, GeminiOCR
 from .extractor import ImageExtractor, ExtractedImage
 from .renderer import MarkdownRenderer, ProcessedImage, RenderResult
 
@@ -12,6 +12,7 @@ __all__ = [
     "ProcessResult",
     "OCREngine",
     "TesseractOCR",
+    "GeminiOCR",
     "ImageExtractor",
     "ExtractedImage",
     "MarkdownRenderer",
